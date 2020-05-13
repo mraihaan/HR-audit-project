933365b51435ae24369a549d585a5eda9636a6f2
mraihaan <56098973+mraihaan@users.noreply.github.com>
2020-05-13 14:55:28 -0400

    Add files via upload

---
+++ psProject.ps1
@@ -0,0 +1,31 @@
+﻿#Author: Mohammad Syed Raihaan
+#NOTE: CSV file path is an arbitrary name
+
+#Importing Active-Directory module
+Import-Module ActiveDirectory
+
+#Variable called 'list' to store contents of CSV file
+$list = Import-Csv -Path filePath123.csv
+
+#Loop through each user in list
+Foreach($user in $list){
+    #variable to store name of user {'name' is header name of the users column in the CSV file}
+    $dn = $user.name
+    #Try the following code block
+    try{
+        #If user doesn't exist in AD
+        if((Get-ADUser -Filter {displayName -like $dn}) -eq $null){
+            #Print out name with nothing next to it
+            Write-Host $user.name
+        }
+        #If user does exist in AD
+        else{
+            #Print out name and enabled status (true or false)
+            Get-ADUser -Filter {displayName -like $dn} | select Name, Enabled
+        }
+    }
+    #If there is an error
+    catch{
+        Write-Host ""
+    }
+}
